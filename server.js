f30a978696023d9903292e6702803abc81fd04da
Lesly <leslyr562@gmail.com>
2021-12-02 01:06:28 -0600

    added connection

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3004
+
+const sequelize = require('./config/connection');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+
+sequelize.syn({ force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
